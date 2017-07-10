231f827c8f2030c8384a224b7f4836aeb662a42b
DreadMoirai <DreadMoirai@gmail.com>
2017-07-09 20:43:28 -0700

    Update CopyRight

--- src/main/java/net/breadmoirai/samurai7/core/Database.java
+++ src/main/java/net/breadmoirai/samurai7/core/Database.java
@@ -12,9 +12,7 @@
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
- *
  */
-
 package net.breadmoirai.samurai7.core;
 
 import org.jdbi.v3.core.Handle;
