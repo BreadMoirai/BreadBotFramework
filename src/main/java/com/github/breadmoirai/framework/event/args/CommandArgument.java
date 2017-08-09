3789d7bb0bf28022a43b8ffb02113c12da8a0469
DreadMoirai <dreadmoirai@gmail.com>
2017-08-09 15:52:44 -0700

    Almost done javadoccing

--- src/main/java/com/github/breadmoirai/framework/event/args/CommandArgument.java
+++ src/main/java/com/github/breadmoirai/framework/event/args/CommandArgument.java
@@ -16,7 +16,10 @@ package com.github.breadmoirai.framework.event.args;
 
 import com.github.breadmoirai.framework.event.Arguments;
 import com.github.breadmoirai.framework.util.Emoji;
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.entities.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,6 +29,15 @@ import java.util.stream.IntStream;
 public interface CommandArgument {
 
     String getString();
+    JDA getJDA();
+    Guild getGuild();
+
+    /**
+     * This is a getter method.
+     * @return The channel this argument was sent in.
+     * @see com.github.breadmoirai.framework.event.args.CommandArgument#getTextChannel
+     */
+    TextChannel getChannel();
 
     /**
      * Invocation is exactly the same as:
@@ -138,16 +150,12 @@ public interface CommandArgument {
      *
      * @return an ordered {@link java.util.stream.IntStream}. If {@link com.github.breadmoirai.framework.event.args.CommandArgument#isRange isRange()} would return false, An empty IntStream will be returned.
      */
+    @NotNull
     IntStream getRange();
 
     /**
-     * Checks if this matches a hexadecimal number
-     *
-     * Checks whether this argument consists of digits 0-10 and letters a-f optional prefixed by {@code #} or {@code 0x}
-     * <p>Result is equivalent to a regex of:
-     * <pre><code>
-     *     (#|0x)?[0-9a-fA-F]+
-     * </code></pre>
+     * Checks if this matches a hexadecimal number, specifically whether this argument consists of digits 0-10 and/or letters a-f optionally prefixed by {@code #} or {@code 0x}
+     * The result of this method is equivalent to checking this argument against a regex of {@code (#|0x)?[0-9a-fA-F]+}
      *
      * @return {@code true} if it matches the format required.
      */
@@ -162,10 +170,7 @@ public interface CommandArgument {
 
     /**
      * Checks if this argument is a {@link net.dv8tion.jda.core.entities.User} mention.
-     * <p>Result is equivalent to a regex of:
-     * <pre><code>
-     *     <(!)?[0-9]+>
-     * </code></pre>
+     * The result of this method is equivalent to checking this argument against a regex of {@code <@(!)?[0-9]+>}
      *
      * @return {@code true} if this is a formatted {@link net.dv8tion.jda.core.entities.User} mention.
      */
@@ -192,42 +197,105 @@ public interface CommandArgument {
      * The {@link net.dv8tion.jda.core.entities.Member} if it can be resolved.
      * @return The {@link net.dv8tion.jda.core.entities.Member} if found, otherwise {@code null}
      */
+    @Nullable
     Member getMember();
 
     /**
-     * Searches for a member in the guild using the argument as criteria.
+     * Searches for a member in the {@link net.dv8tion.jda.core.entities.Guild} using the argument as criteria.
      * If it matches multiple users, the user whose name begins with the argument is given precedence. If multiple users match, the first one found is returned.
      * This attempts to match Username and Nickname.
-     * @return an <code>{@link java.util.Optional}<{@link net.dv8tion.jda.core.entities.Member}></code>
+     * <p>If this argument is already a valid {@link net.dv8tion.jda.core.entities.Member} mention, that {@link net.dv8tion.jda.core.entities.Member} is returned within the {@link java.util.Optional}.
+     * @return the first {@link net.dv8tion.jda.core.entities.Member} found, otherwise an empty {@link java.util.Optional}
      */
+    @NotNull
     Optional<Member> findMember();
 
     /**
      * Searches for members whose Username or Nickname contains this argument.
+     * <p>If this argument is already a valid {@link net.dv8tion.jda.core.entities.Member} mention, a {@link java.util.List} with only that element is returned.
      * @return A never-null {@link java.util.List} of {@link net.dv8tion.jda.core.entities.Member Members}
      */
+    @NotNull
     List<Member> searchMembers();
 
+    /**
+     * Checks if this argument is a {@link net.dv8tion.jda.core.entities.Role} mention.
+     * The result of this method is equivalent to checking this argument against a regex of {@code <@&[0-9]+>}
+     * @return {@code true} if this is a correctly formatted {@link net.dv8tion.jda.core.entities.Role} mention
+     */
     boolean isRole();
 
+    /**
+     * Attempts to resolve this argument to a {@link net.dv8tion.jda.core.entities.Role} in the {@link net.dv8tion.jda.core.entities.Guild}.
+     * @return {@link net.dv8tion.jda.core.entities.Role} if role is present within the {@link net.dv8tion.jda.core.entities.Guild}, otherwise {@code null}
+     */
+    @Nullable
     Role getRole();
 
+    /**
+     * Attempts to match this argument to a {@link net.dv8tion.jda.core.entities.Role} by name.
+     * <p>If this argument is already a valid {@link net.dv8tion.jda.core.entities.Role} mention, that {@link net.dv8tion.jda.core.entities.Role} is returned within the {@link java.util.Optional}.
+     * @return the first {@link net.dv8tion.jda.core.entities.Role} found, otherwise an empty {@link java.util.Optional}
+     */
+    @NotNull
     Optional<Role> findRole();
 
+    /**
+     * Returns a {@link java.util.List} of {@link net.dv8tion.jda.core.entities.Role Roles} that match this argument. The criteria being that the {@link net.dv8tion.jda.core.entities.Role} name should contain this argument.
+     * <p>If this argument is already a valid {@link net.dv8tion.jda.core.entities.Role} mention, a {@link java.util.List} with only that element is returned.
+     * @return A never-null {@link java.util.List} of {@link net.dv8tion.jda.core.entities.Role Roles}.
+     */
+    @NotNull
     List<Role> searchRoles();
 
+    /**
+     * Checks if this argument is a {@link net.dv8tion.jda.core.entities.TextChannel} mention.
+     * The result of this method is equivalent to checking this argument against a regex of {@code <#[0-9]+>}
+     * @return {@code true} if this is a correctly formatted {@link net.dv8tion.jda.core.entities.TextChannel} mention
+     */
     boolean isTextChannel();
 
+    /**
+     * Attempts to resolve this argument as a {@link net.dv8tion.jda.core.entities.TextChannel} mention to a {@link net.dv8tion.jda.core.entities.TextChannel} in the {@link net.dv8tion.jda.core.entities.Guild}.
+     * @return {@link net.dv8tion.jda.core.entities.Role} role is present within the {@link net.dv8tion.jda.core.entities.Guild}, otherwise {@code null}
+     */
+    @Nullable
     TextChannel getTextChannel();
 
+    /**
+     * Attempts to match this argument to a {@link net.dv8tion.jda.core.entities.TextChannel} by name.
+     * <p>If this argument is already a valid {@link net.dv8tion.jda.core.entities.TextChannel} mention, that {@link net.dv8tion.jda.core.entities.TextChannel} is returned within the {@link java.util.Optional}.
+     * @return the first {@link net.dv8tion.jda.core.entities.TextChannel} found, otherwise an empty {@link java.util.Optional}
+     */
+    @NotNull
     Optional<TextChannel> findTextChannel();
 
+    /**
+     * Returns a {@link java.util.List} of {@link net.dv8tion.jda.core.entities.TextChannel TextChannels} that match this argument. The criteria being that the {@link net.dv8tion.jda.core.entities.TextChannel} name should contain this argument.
+     * <p>If this argument is already a valid {@link net.dv8tion.jda.core.entities.TextChannel} mention, a {@link java.util.List} with only that element is returned.
+     * @return A never-null {@link java.util.List} of {@link net.dv8tion.jda.core.entities.TextChannel TextChannels}.
+     */
+    @NotNull
     List<TextChannel> searchTextChannels();
 
+    /**
+     * Attempts to match this argument to a {@link net.dv8tion.jda.core.entities.VoiceChannel} by name.
+     * @return the first {@link net.dv8tion.jda.core.entities.VoiceChannel} if found, otherwise an empty {@link java.util.Optional}
+     */
+    @NotNull
     Optional<VoiceChannel> findVoiceChannel();
 
+    /**
+     * Returns a {@link java.util.List} of {@link net.dv8tion.jda.core.entities.VoiceChannel Roles} that match this argument. The criteria being that the {@link net.dv8tion.jda.core.entities.VoiceChannel} name should contain this argument.
+     * @return A never-null {@link java.util.List} of {@link net.dv8tion.jda.core.entities.VoiceChannel VoiceChannels}.
+     */
+    @NotNull
     List<VoiceChannel> searchVoiceChannels();
 
+    /**
+     * Will attempt to match this argument against the emojis found in {@link com.github.breadmoirai.framework.util.Emoji} using their unicode value.
+     * @return {@code true if }
+     */
     boolean isEmoji();
 
     Emoji getEmoji();
