cc838b124e43bc1add40e0c5efca23617839dab7
DreadMoirai <dreadmoirai@gmail.com>
2017-08-26 14:13:37 -0700

    Moved ArgumentFlags back to it's own class

--- src/main/java/com/github/breadmoirai/bot/framework/event/args/ArgumentFlags.java
+++ src/main/java/com/github/breadmoirai/bot/framework/event/args/ArgumentFlags.java
@@ -1,9 +1,86 @@
 package com.github.breadmoirai.bot.framework.event.args;
 
+import gnu.trove.map.TObjectIntMap;
+import gnu.trove.map.hash.TObjectIntHashMap;
+
 /**
  * Holds flags for ArgumentParsing.
  */
-public class ArgumentFlags {
+public final class ArgumentFlags {
+    /**
+     * Signifies that the {@link com.github.breadmoirai.bot.framework.event.args.ArgumentMapper} should do everything it can to map to it's type. This has no effect on numeric types.
+     */
+    public static final String STRICT = "strict";
+    /**
+     * Signifies that this numeric argument is in hexadecimal form.
+     */
+    public static final String HEX = "hex";
 
+    private static final TObjectIntMap<String> CUSTOM_FLAGS;
+
+    static {
+        CUSTOM_FLAGS = new TObjectIntHashMap<>();
+        registerFlag(STRICT);
+        registerFlag(HEX);
+    }
+
+    public static void registerFlag(String flagName) {
+        CUSTOM_FLAGS.putIfAbsent(flagName.toLowerCase(), 0b1 << CUSTOM_FLAGS.size());
+    }
+
+    public static int addFlag(int flags, String... flagNames) {
+        int acc = flags;
+        for (String flagName : flagNames) {
+            final int flag = getFlag(flagName);
+            if (flag == 0) System.err.println("No such flag: " + flagName);
+            else acc = acc | flag;
+        }
+        return acc;
+    }
+
+    /**
+     * Uses a bit-wise OR to combine the specified flag. If a flag is not found, a warning is issued.
+     *
+     * @param flagNames
+     * @return
+     */
+    public static int getFlags(String... flagNames) {
+        return addFlag(0, flagNames);
+    }
+
+    /**
+     * Gets the bit indicator of the flag.
+     * @param flagName the name of the flag
+     * @return the value if found, otherwise {@code 0}
+     */
+    public static int getFlag(String flagName) {
+        return CUSTOM_FLAGS.get(flagName.toLowerCase());
+    }
+
+    /**
+     * {@code true} means that the {@link CommandArgument} should only be matched to this type when explicitly specified such as a Mention.
+     * <p>If a flag is NOT strict, The mapper should try it's best to match it to the type provided such as searching for a matching name in case of a User or Channel.
+     *
+     * @param flags the int value.
+     * @return {@code true} if the flag indicates to use a Strict Strategy.
+     */
+    public static boolean isStrict(int flags) {
+        return hasFlag(STRICT, flags);
+    }
+
+    /**
+     * checks if the provided {@code int} value contains the specified flag.
+     * @param flagName the name of the flag.
+     * @param flags the flag value to test.
+     * @return {@code true} if the value contains the specified flag, otherwise {@code false}. If the given {@code flagName} is not registered, {@code false} will be returned.
+     */
+    public static boolean hasFlag(String flagName, int flags) {
+        return (getFlag(flagName) & flags) != 0;
+    }
+
+    /**
+     * A private constructor for a static class.
+     */
+    private ArgumentFlags() {}
 
 }
--- src/main/java/com/github/breadmoirai/bot/framework/event/args/ArgumentTypes.java
+++ src/main/java/com/github/breadmoirai/bot/framework/event/args/ArgumentTypes.java
@@ -17,8 +17,6 @@ package com.github.breadmoirai.bot.framework.event.args;
 import com.github.breadmoirai.bot.framework.event.Arguments;
 import com.github.breadmoirai.bot.framework.event.args.impl.ArgumentTypeSimpleImpl;
 import com.github.breadmoirai.bot.util.Emoji;
-import gnu.trove.map.TObjectIntMap;
-import gnu.trove.map.hash.TObjectIntHashMap;
 import net.dv8tion.jda.core.entities.*;
 
 import java.util.HashMap;
@@ -31,7 +29,7 @@ import java.util.stream.IntStream;
 /**
  * Does argument Mapping.
  */
-public class ArgumentTypes {
+public final class ArgumentTypes {
     public static final Class<Integer> INTEGER = Integer.TYPE;
     public static final Class<Long> LONG = Long.TYPE;
     public static final Class<Float> FLOAT = Float.TYPE;
@@ -46,29 +44,16 @@ public class ArgumentTypes {
     public static final Class<Emoji> EMOJI = Emoji.class;
 
     private static final Map<Class<?>, ArgumentMapper<?>> map;
-    /**
-     * Signifies that the {@link com.github.breadmoirai.bot.framework.event.args.ArgumentMapper} should do everything it can to map to it's type. This has no effect on numeric types.
-     */
-    public static final String STRICT = "strict";
-    /**
-     * Signifies that this numeric argument is in hexadecimal form.
-     */
-    public static final String HEX = "hex";
 
-    private static final TObjectIntMap<String> CUSTOM_FLAGS;
     private static final ArgumentMapper<Void> VOID_TYPE;
 
     static {
-        CUSTOM_FLAGS = new TObjectIntHashMap<>();
-        registerFlag(STRICT);
-        registerFlag(HEX);
-
         map = new HashMap<>();
 
         VOID_TYPE = (arg, flags) -> Optional.empty();
 
         final ArgumentMapper<Integer> intType = (arg, flags) -> {
-            final boolean hex = ArgumentTypes.hasFlag(HEX, flags);
+            final boolean hex = ArgumentFlags.hasFlag(ArgumentFlags.HEX, flags);
             if (hex && arg.isHex()) {
                 try {
                     return Optional.of(Integer.parseInt(Arguments.stripHexPrefix(arg.getArgument()), 16));
@@ -84,7 +69,7 @@ public class ArgumentTypes {
         map.put(Integer.class, intType);
 
         final ArgumentMapper<Long> longType = (arg, flags) -> {
-            final boolean hex = ArgumentTypes.hasFlag(HEX, flags);
+            final boolean hex = ArgumentFlags.hasFlag(ArgumentFlags.HEX, flags);
             if (hex && arg.isHex()) {
                 try {
                     return Optional.of(Long.parseLong(Arguments.stripHexPrefix(arg.getArgument()), 16));
@@ -118,7 +103,7 @@ public class ArgumentTypes {
         map.put(Boolean.class, boolType);
 
         registerArgumentMapper(RANGE, (arg, flags) -> {
-            if ((ArgumentTypes.isStrict(flags) && arg.isInteger())
+            if ((ArgumentFlags.isStrict(flags) && arg.isInteger())
                     || (arg.isRange() || arg.isInteger())) {
                 return Optional.of(arg.parseRange());
             }
@@ -126,35 +111,59 @@ public class ArgumentTypes {
         });
 
         registerArgumentMapper(USER, (arg, flags) -> {
-            if (ArgumentTypes.isStrict(flags)) {
+            if (ArgumentFlags.isStrict(flags)) {
                 return arg.isValidUser() ? Optional.of(arg.getUser()) : Optional.empty();
-            } else {
-                return arg.findMember().map(Member::getUser);
             }
+            if (arg.isLong()) {
+                long l = arg.parseLong();
+                User user = arg.getJDA().getUserById(l);
+                if (user != null) {
+                    return Optional.of(user);
+                }
+            }
+            return arg.findMember().map(Member::getUser);
         });
 
         registerArgumentMapper(MEMBER, (arg, flags) -> {
-            if (ArgumentTypes.isStrict(flags)) {
+            if (ArgumentFlags.isStrict(flags)) {
                 return arg.isValidMember() ? Optional.of(arg.getMember()) : Optional.empty();
-            } else {
-                return arg.findMember();
             }
+            if (arg.isLong()) {
+                long l = arg.parseLong();
+                Member member = arg.getGuild().getMemberById(l);
+                if (member != null) {
+                    return Optional.of(member);
+                }
+            }
+            return arg.findMember();
         });
 
         registerArgumentMapper(ROLE, (arg, flags) -> {
-            if (ArgumentTypes.isStrict(flags)) {
+            if (ArgumentFlags.isStrict(flags)) {
                 return arg.isValidRole() ? Optional.of(arg.getRole()) : Optional.empty();
-            } else {
-                return arg.findRole();
             }
+            if (arg.isLong()) {
+                long l = arg.parseLong();
+                Role role = arg.getGuild().getRoleById(l);
+                if (role != null) {
+                    return Optional.of(role);
+                }
+            }
+            return arg.findRole();
         });
 
         registerArgumentMapper(TEXTCHANNEL, (arg, flags) -> {
-            if (ArgumentTypes.isStrict(flags)) {
+            if (ArgumentFlags.isStrict(flags)) {
                 return arg.isValidTextChannel() ? Optional.of(arg.getTextChannel()) : Optional.empty();
-            } else {
-                return arg.findTextChannel();
             }
+            if (arg.isLong()) {
+                long l = arg.parseLong();
+                TextChannel channel = arg.getJDA().getTextChannelById(l);
+                if (channel != null) {
+                    return Optional.of(channel);
+                }
+            }
+            return arg.findTextChannel();
         });
 
         registerArgumentMapper(EMOTE, (arg, flags) -> arg.isEmote() ? Optional.of(arg.getEmote()) : Optional.empty());
@@ -187,6 +196,34 @@ public class ArgumentTypes {
     }
 
     /**
+     * Attempts to map the CommandArgument to the type provided using the flags and a registered ArgumentMapper.
+     * If an ArgumentMapper is not registered with the type requested, an empty Optional is returned.
+     *
+     * @param type  the intended type class
+     * @param arg   the argument to map from
+     * @param flags any {@link com.github.breadmoirai.bot.framework.event.args.ArgumentFlags flags}
+     * @param <T>   the type
+     * @return an Optional containing the result if successful. Otherwise empty.
+     */
+    public static <T> Optional<T> getAsType(Class<T> type, CommandArgument arg, int flags) {
+        //noinspection unchecked
+        return (Optional<T>) map.getOrDefault(type, VOID_TYPE).map(arg, flags);
+    }
+
+    /**
+     * Attempts to map the CommandArgument to the type provided using the flags and a registered ArgumentMapper.
+     * If an ArgumentMapper is not registered with the type requested, an empty Optional is returned.
+     *
+     * @param type the intended type class
+     * @param arg  the argument to map from
+     * @param <T>  the type
+     * @return an Optional containing the result if successful. Otherwise empty.
+     */
+    public static <T> Optional<T> getAsType(Class<T> type, CommandArgument arg) {
+        return getAsType(type, arg, 0);
+    }
+
+    /**
      * Implemented as
      * <pre><code>
      *     {@link com.github.breadmoirai.bot.framework.event.args.ArgumentTypes#getAsType(java.lang.Class, com.github.breadmoirai.bot.framework.event.args.CommandArgument, int) getAsType(type, arg, flags)}.isPresent();
@@ -197,70 +234,14 @@ public class ArgumentTypes {
     }
 
     /**
-     * Attempts to map the CommandArgument to the type provided using the flags and a registered ArgumentMapper.
-     * If an ArgumentMapper is not registered with the type requested, an empty Optional is returned.
-     *
-     * @param type  the intended type class
-     * @param arg   the argument to map from
-     * @param flags any flags.
-     * @param <T>   the type
-     * @return an Optional containing the result if successful. Otherwise empty.
+     * Implemented as
+     * <pre><code>
+     *     {@link com.github.breadmoirai.bot.framework.event.args.ArgumentTypes#getAsType(java.lang.Class, com.github.breadmoirai.bot.framework.event.args.CommandArgument) getAsType(type, arg, flags)}.isPresent();
+     * </code></pre>
      */
-    public static <T> Optional<T> getAsType(Class<T> type, CommandArgument arg, int flags) {
-        //noinspection unchecked
-        return (Optional<T>) map.getOrDefault(type, VOID_TYPE).map(arg, flags);
-    }
-
     public static boolean isOfType(Class<?> type, CommandArgument arg) {
         return isOfType(type, arg, 0);
     }
 
-    public static <T> Optional<T> getAsType(Class<T> type, CommandArgument arg) {
-        return getAsType(type, arg, 0);
-    }
 
-    public static void registerFlag(String flagName) {
-        CUSTOM_FLAGS.putIfAbsent(flagName.toLowerCase(), 0b1 << CUSTOM_FLAGS.size());
-    }
-
-    public static int addFlag(int flags, String... flagNames) {
-        int acc = flags;
-        for (String flagName : flagNames) {
-            final int flag = getFlag(flagName);
-            if (flag == 0) System.err.println("No such flag: " + flagName);
-            else acc = acc | flag;
-        }
-        return acc;
-    }
-
-    /**
-     * Uses a bit-wise OR to combine the specified flag. If a flag is not found, a warning is issued.
-     *
-     * @param flagNames
-     * @return
-     */
-    public static int flags(String... flagNames) {
-        return addFlag(0, flagNames);
-    }
-
-    public static int getFlag(String flagName) {
-        return CUSTOM_FLAGS.get(flagName.toLowerCase());
-    }
-
-    /**
-     * {@code true} means that the
-     * {@link CommandArgument} should only be matched
-     * to this type when explicitly specified such as a Mention.
-     * <p>If a flag is NOT strict, The mapper should try it's best to match it to the type provided such as searching for a matching name in case of a User or Channel.
-     *
-     * @param flags the int value.
-     * @return {@code true} if the flag indicates to use a Strict Strategy.
-     */
-    public static boolean isStrict(int flags) {
-        return !hasFlag(STRICT, flags);
-    }
-
-    public static boolean hasFlag(String flagName, int flags) {
-        return (getFlag(flagName) & flags) != 0;
-    }
 }
