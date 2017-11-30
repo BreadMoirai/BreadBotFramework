0bba11428490032e19bbff9163b5fd04eabe682f
BreadMoirai <dreadmoirai@gmail.com>
2017-11-29 16:36:54 -0800

    Update Copyrights

--- src/test/java/com/github/breadmoirai/tests/client/ClientTest.java
+++ src/test/java/com/github/breadmoirai/tests/client/ClientTest.java
@@ -1,17 +1,20 @@
-package com.github.breadmoirai.tests.client;/*    Copyright 2017 Ton Ly
- 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
- 
-      http://www.apache.org/licenses/LICENSE-2.0
- 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
+/*
+ *        Copyright 2017 Ton Ly (BreadMoirai)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.github.breadmoirai.tests.client;
 
 import com.github.breadmoirai.breadbot.framework.BreadBotClient;
 import com.github.breadmoirai.breadbot.framework.CommandEvent;
@@ -19,7 +22,6 @@ import com.github.breadmoirai.breadbot.framework.builder.BreadBotClientBuilder;
 import com.github.breadmoirai.breadbot.framework.builder.CommandHandleBuilder;
 import com.github.breadmoirai.breadbot.framework.internal.event.CommandEventFactoryImpl;
 import com.github.breadmoirai.breadbot.modules.prefix.DefaultPrefixModule;
-import com.github.breadmoirai.breadbot.util.DiscordPatterns;
 import com.github.breadmoirai.breadbot.util.Emoji;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
@@ -253,9 +255,9 @@ public class ClientTest {
 //        when(mockInput.getGuild()).thenReturn(mockGuild);
 //
 
-        final String[] split = DiscordPatterns.WHITE_SPACE.split(input.substring(1), 2);
-        final String key = split[0];
-        final String content = split.length > 1 ? split[1].trim() : null;
+//        final String[] split = DiscordPatterns.WHITE_SPACE.split(input.substring(1), 2);
+//        final String key = split[0];
+//        final String content = split.length > 1 ? split[1].trim() : null;
 
         CommandEventFactoryImpl eventFactory = new CommandEventFactoryImpl(new DefaultPrefixModule("!"));
 
