cde58d0392bf18a7e666064285c194c0d1e4d6cd
DreadMoirai <dreadmoirai@gmail.com>
2018-01-20 13:38:01 -0800

    addSetters to abstract command

--- src/main/java/com/github/breadmoirai/breadbot/framework/command/AbstractCommand.java
+++ src/main/java/com/github/breadmoirai/breadbot/framework/command/AbstractCommand.java
@@ -3,6 +3,7 @@ package com.github.breadmoirai.breadbot.framework.command;
 import com.github.breadmoirai.breadbot.framework.event.CommandEvent;
 import com.github.breadmoirai.breadbot.framework.event.internal.CommandEventInternal;
 
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -10,6 +11,8 @@ public abstract class AbstractCommand implements Command {
 
     protected String[] keys;
     protected String name, group, description;
+    private Map<String, Command> children;
+    private Command parent;
 
     public abstract void onCommand(CommandEvent event);
 
@@ -41,12 +44,42 @@ public abstract class AbstractCommand implements Command {
 
     @Override
     public Map<String, Command> getChildren() {
-        return null;
+        return children;
     }
 
     @Override
     public Command getParent() {
-        return null;
+        return parent;
+    }
+
+    protected void setParent(Command parent) {
+        this.parent = parent;
+    }
+
+    protected void setKeys(String... keys) {
+        this.keys = keys;
+    }
+
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    protected void setGroup(String group) {
+        this.group = group;
+    }
+
+    protected void setDescription(String description) {
+        this.description = description;
+    }
+
+    protected void addChild(AbstractCommand command) {
+        if (children == null) {
+            children = new HashMap<>();
+        }
+        for (String s : command.getKeys()) {
+            children.put(s, command);
+        }
+        command.setParent(this);
     }
 
 }
