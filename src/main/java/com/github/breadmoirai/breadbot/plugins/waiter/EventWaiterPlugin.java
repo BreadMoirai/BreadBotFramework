2c8d68e3aceb832f81fd86aea2238d5b72db3f87
DreadMoirai <dreadmoirai@gmail.com>
2018-01-28 20:15:21 -0800

    remove CommandPlugin#help. Updated EventWaiterPlugin

--- src/main/java/com/github/breadmoirai/breadbot/plugins/waiter/EventWaiterPlugin.java
+++ src/main/java/com/github/breadmoirai/breadbot/plugins/waiter/EventWaiterPlugin.java
@@ -3,18 +3,27 @@ package com.github.breadmoirai.breadbot.plugins.waiter;
 import com.github.breadmoirai.breadbot.framework.CommandPlugin;
 import com.github.breadmoirai.breadbot.framework.builder.BreadBotBuilder;
 import net.dv8tion.jda.core.events.Event;
+import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.hooks.EventListener;
 
 public class EventWaiterPlugin implements CommandPlugin, EventListener {
 
-    @Override
-    public void initialize(BreadBotBuilder builder) {
+    private final EventWaiter eventWaiter;
 
+    public EventWaiterPlugin() {
+        eventWaiter = new EventWaiter();
     }
 
+    @Override
+    public void initialize(BreadBotBuilder builder) {
+        builder.bindTypeModifier(EventWaiter.class, p -> p.setParser((parameter, list, parser) -> eventWaiter));
+    }
 
     @Override
     public void onEvent(Event event) {
-
+        if (event instanceof ReadyEvent) {
+            event.getJDA().addEventListener(eventWaiter);
+            event.getJDA().removeEventListener(this);
+        }
     }
 }
