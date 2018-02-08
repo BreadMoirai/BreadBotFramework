08bb80f560da3dc39704edd8ea0df49bea82a8ad
DreadMoirai <dreadmoirai@gmail.com>
2018-02-07 23:36:24 -0800

    add tests for Duration mapping

--- src/test/java/com/github/breadmoirai/tests/DefaultTester.java
+++ src/test/java/com/github/breadmoirai/tests/DefaultTester.java
@@ -21,6 +21,7 @@ import com.github.breadmoirai.breadbot.framework.builder.BreadBotBuilder;
 import com.github.breadmoirai.breadbot.framework.event.internal.CommandEventInternal;
 import com.github.breadmoirai.tests.commands.AuthorCommand;
 import com.github.breadmoirai.tests.commands.EchoCommand;
+import com.github.breadmoirai.tests.commands.TimeCommand;
 import com.github.breadmoirai.tests.commands.WhoAmICommand;
 import org.junit.Test;
 import uk.org.lidalia.slf4jext.Level;
@@ -69,6 +70,20 @@ public class DefaultTester {
         assertResponse(MockFactory.UserType.BASIC, "!isadmin", null);
         assertResponse(MockFactory.UserType.ADMIN, "!isadmin", "yes");
         assertResponse(MockFactory.UserType.CREATOR, "!isadmin", "yes");
+    }
+
+    @Test
+    public void timeTest() {
+        client = new BreadBotBuilder()
+                .addCommand(TimeCommand::new)
+                .addCommand(TimeCommand::new, command -> command.setKeys("d2").getParameter(0).setWidth(2))
+                .build();
+        assertResponse("!d 1m", "PT1M");
+        assertResponse("!d 1m 1h 1s", "PT1H1M1S");
+        assertResponse("!d 1day", "PT24H");
+        assertResponse("!d2 1 day 5 hours 10 minutes 13 seconds", "PT29H10M13S");
+        assertResponse("!d awff2 70s 21ttg1 4h 1raf", "PT4H1M10S");
+        assertResponse("!d2 hello 2 sec my name is 4 h", "PT4H2S");
     }
 
     private void assertResponse(String input, String expected) {
