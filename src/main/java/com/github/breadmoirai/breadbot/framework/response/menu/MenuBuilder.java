b9bd1ff5f9caae62f5ec2a9c88e0467388130091
breadmoirai <breadmoirai@gmail.com>
2018-04-20 21:07:33 -0700

    remove Menus from branch
    Signed-off-by: breadmoirai <breadmoirai@gmail.com>

--- src/main/java/com/github/breadmoirai/breadbot/framework/response/menu/MenuBuilder.java
+++
@@ -1,5 +0,0 @@
-package com.github.breadmoirai.breadbot.framework.response.menu;
-
-public class MenuBuilder {
-
-}
--- src/main/java/com/github/breadmoirai/breadbot/framework/response/menu/MenuResponse.java
+++
@@ -1,7 +0,0 @@
-package com.github.breadmoirai.breadbot.framework.response.menu;
-
-import com.github.breadmoirai.breadbot.framework.response.DynamicCommandResponse;
-
-public abstract class MenuResponse implements DynamicCommandResponse {
-
-}
